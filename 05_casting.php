25f4647078050e508684e0a64a0fccb34a4f0fbd
MarioJGC <77269474mario@gmail.com>
2024-08-28 21:00:21 -0500

    Add file Change Data Type

---
+++ 05_casting.php
@@ -0,0 +1,63 @@
+<?php
+
+    // Change Data Type 
+
+    /*
+    (string) - Converts to data type String
+    (int) - Converts to data type Integer
+    (float) - Converts to data type Float
+    (bool) - Converts to data type Boolean
+    (array) - Converts to data type Array
+    (object) - Converts to data type Object
+    (unset) - Converts to data type NULL
+    */
+
+    // Cast to string
+    $a = 5;       // Integer
+    $b = 5.34;    // Float
+    $c = "hello"; // String
+    $d = true;    // Boolean
+    $e = NULL;    // NULL
+
+    $a = (string) $a;
+    $b = (string) $b;
+    $c = (string) $c;
+    $d = (string) $d;
+    $e = (string) $e;
+
+    // Cast to integer
+    $a = (int) $a;
+    $b = (int) $b;
+    $c = (int) $c;
+    $d = (int) $d;
+    $e = (int) $e;
+
+    // Cast to Float
+    $a = (float) $a;
+    $b = (float) $b;
+    $c = (float) $c;
+    $d = (float) $d;
+    $e = (float) $e;
+
+    // Cast to boolean
+    $a = (bool) $a;
+    $b = (bool) $b;
+    $c = (bool) $c;
+    $d = (bool) $d;
+    $e = (bool) $e;
+
+    // Cast to array
+    $a = (array) $a;
+    $b = (array) $b;
+    $c = (array) $c;
+    $d = (array) $d;
+    $e = (array) $e;
+
+    // Cast to object
+    $a = (object) $a;
+    $b = (object) $b;
+    $c = (object) $c;
+    $d = (object) $d;
+    $e = (object) $e;
+
+?>
