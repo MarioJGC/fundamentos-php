7e87eb4fc30ef4854df589bd77f859a2067bf1fb
MarioJGC <77269474mario@gmail.com>
2024-08-29 15:43:34 -0500

    Add file functions

---
+++ 12_functions.php
@@ -0,0 +1,18 @@
+<?php
+
+    // Create a Function
+    function hello(){
+        echo "Hello World!";
+    }
+
+    // Call a Function
+    hello();
+
+    // PHP Function Arguments
+    function hear($fname){
+        echo "$fname hear!";
+    }
+
+    hear("Josh");
+
+?>
