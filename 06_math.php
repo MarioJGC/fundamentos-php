9c57c96b823125f1b01d03d850f439c4312718e0
MarioJGC <77269474mario@gmail.com>
2024-08-28 21:24:52 -0500

    Add file math

---
+++ 06_math.php
@@ -0,0 +1,21 @@
+<?php
+
+    // PHP has a set of math functions that allows you to perform mathematical tasks on numbers.
+
+    // PHP pi() function
+    echo (pi());
+
+    // PHP min() and max() functions
+    echo (min(0, 150, 30, 20, -8, -200));
+    echo (max(0, 150, 30, 20, -8, -200));
+
+    // PHP abs() function
+    echo (abs(-2));
+
+    // PHP sqrt() function
+    echo (sqrt(64));
+
+    // PHP rand() functions
+    echo (rand());
+
+?>
