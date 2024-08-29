4a9289ab3f827f76b6ad82b840a8f3dfdb33625b
MarioJGC <77269474mario@gmail.com>
2024-08-29 15:38:54 -0500

    Add file loops

---
+++ 11_loops.php
@@ -0,0 +1,35 @@
+<?php
+
+    //  while Loop
+    $i = 1;
+    while($i < 7){
+        echo $i;
+        $i++;
+    }
+
+    // for Loop
+    for ($x = 0; $x <= 10; $x++) {
+        echo "The number is: $x <br>";
+      }
+
+    // foreach Loop
+    $colors = array("red", "green", "blue", "yellow");
+    foreach($colors as $c){
+        echo ($c);
+    }
+
+    // break
+    foreach ($colors as $x) {
+    if ($x == "blue") break;
+    echo "$x <br>";
+    }
+
+    // continue
+    for ($x = 0; $x < 10; $x++) {
+        if ($x == 4) {
+          continue;
+        }
+        echo "The number is: $x <br>";
+    }
+
+?>
